5849af52f8dc757e01213f632d282db40a5c59ec
이희영 <gl171111@gmail.com>
2020-02-11 09:29:17 +0900

    모든 컴포넌트가 로드되기 때문에 공유하는 모달 컴포넌트를 Main컴포넌트에서 호출하도록 함

--- src/layouts/main/Main.vue
+++ src/layouts/main/Main.vue
@@ -10,17 +10,81 @@
               <router-view></router-view>
             </div>
           </div>
-          
         </main>
+        <!-- modal - 새폴더 -->
+        <!-- <ModalInput title="새폴더" ok="만들기" v-if="isAddFolder" @close="isAddFolder=false" @submit="onAddFolder"/> -->
+        <!-- modal - 이름바꾸기 -->
+        <ModalInput title="이름 바꾸기" ok="확인" :inputOriginal="inputOriginal" v-if="isOpenRenameModal" @submit="onRename" @close="SET_IS_OPEN_RENAME_MODAL(false)"/>
+        <!-- modal - 이동하기 -->
+        <ModalLocation v-if="isOpenMoveModal" @submit="onMove" @close="SET_IS_OPEN_MOVE_MODAL(false)" />
+        <!-- modal - 복사하기 -->
+        <ModalLocation v-if="isOpenCopyModal" @submit="onCopy" @close="SET_IS_OPEN_COPY_MODAL(false)" />
     </div>
    
 </template>
 
 <script>
-import Header from '../components/Header.vue';
-import Sidebar from '../components/Sidebar.vue';
+import {mapActions, mapMutations, mapState} from 'vuex';
+import Header from '@/layouts/components/Header.vue';
+import Sidebar from '@/layouts/components/Sidebar.vue';
+import ModalInput from "@/components/ModalInput.vue";
+import ModalLocation from "@/components/ModalLocation.vue";
+
 export default {
-    name : 'Main',
-    components: {Header, Sidebar}
+    components: {
+      Header, 
+      Sidebar,
+      ModalInput,
+      ModalLocation
+    },
+    computed :{
+      ...mapState(['currentItem','isOpenCopyModal','isOpenMoveModal','isOpenRenameModal','targetFolderId']),
+      inputOriginal(){
+            if(this.currentItem.type === 'folder'){ return this.currentItem.folderName; }
+            else if(this.currentItem.type === 'file'){ return this.currentItem.fileName; }
+        }
+    },
+    methods : {
+      ...mapMutations(['SET_IS_OPEN_COPY_MODAL', 'SET_IS_OPEN_MOVE_MODAL', 'SET_IS_OPEN_RENAME_MODAL']),
+      ...mapActions(['RENAME_FOLDER','RENAME_FILE','COPY_FOLDER','COPY_FILE','MOVE_FOLDER','MOVE_FILE']),
+        onRename(value){
+            this.inputNewName = value;
+            if(this.currentItem.type === 'folder'){
+                this.RENAME_FOLDER({folderId: this.currentItem.folderId, newFolderName : this.inputNewName}).finally(_ => {
+                this.SET_IS_OPEN_RENAME_MODAL(false); 
+            
+                //여기서 dispatch할 때 fetchfolder할지 fetchstaareddisk할지상황 난누기
+                })
+            }else if(this.currentItem.type === 'file'){
+                this.RENAME_FILE({fileId: this.currentItem.fileId, newFileName : this.inputNewName}).finally(_ => {
+                this.SET_IS_OPEN_RENAME_MODAL(false);
+             })
+            }
+              
+          },
+        onCopy(){
+          if(this.currentItem.type === 'folder'){
+            this.COPY_FOLDER({folderId : this.currentItem.folderId, targetFolderId : this.targetFolderId}).finally(_ => {
+              this.SET_IS_OPEN_COPY_MODAL(false);
+            })
+            
+          }else if(this.currentItem.type === 'file'){
+            this.COPY_FILE({fileId : this.currentItem.fileId, targetFolderId : this.targetFolderId}).finally(_ => {
+              this.SET_IS_OPEN_COPY_MODAL(false);
+            })  
+          }
+        },
+        onMove(){
+          if(this.currentItem.type === 'folder'){
+            this.MOVE_FOLDER({folderId : this.currentItem.folderId, targetFolderId : this.targetFolderId});
+            this.SET_IS_OPEN_MOVE_MODAL(false);
+          }else if(this.currentItem.type === 'file'){
+            this.MOVE_FILE({fileId : this.currentItem.fileId, targetFolderId : this.targetFolderId});
+            this.SET_IS_OPEN_MOVE_MODAL(false);
+          }
+        }
+    }
+    
+    
 }
 </script>
