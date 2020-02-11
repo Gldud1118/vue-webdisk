8a52a2dd3e3db1930bec81b3ca4a3c97b4f2a906
이희영 <gl171111@gmail.com>
2020-02-11 09:32:17 +0900

    삭제된 파일 혹은 폴더만 보여주는 휴지통 컴포넌트

---
+++ src/views/Trash.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="filemgr-wrapper">
+        <div class="filemgr-content">
+            <div class="filemgr-content-body ps ps--active-y">
+                <FolderContents :data="trashedDisk"/>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+import {mapActions, mapMutations, mapState} from 'vuex';
+import FolderContents from "@/components/FolderContents.vue";
+
+export default {
+    components:{
+       FolderContents
+    },
+    computed:{
+        ...mapState(['trashedDisk', 'isTrashedUpdate'])
+    },
+    created(){
+        this.fetchData();
+    },
+    methods: {
+        ...mapActions(['FETCH_TRASHED_DISK']),
+        fetchData(){
+            if(this.isTrashedUpdate){
+                this.FETCH_TRASHED_DISK();
+            }
+        }
+    },
+}
+</script>
