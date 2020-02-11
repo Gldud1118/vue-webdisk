6bedb01db9a73adcc653f01c23e0b1266ff7de8a
이희영 <gl171111@gmail.com>
2020-02-11 12:09:46 +0900

    사이드바 컴포넌트 업데이트

--- src/layouts/components/Sidebar.vue
+++ src/layouts/components/Sidebar.vue
@@ -2,25 +2,20 @@
     <!-- Sidebar -->
     <aside id="main-sidebar" class="dt-sidebar">
         <div class="dt-sidebar__container">
-            <div class="d-flex mb-6" style="padding-left: 30px;">
+            <div class="mb-6" style="padding-left: 30px;">
                 <div class="dropdown-wrapper mr-2">
                     <div class="dropdown">
-                        <button class="btn btn-xs btn-block btn-outline-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" @click.prevent="dropdownToggle">New</button>
-                        <div class="dropdown-menu dropdown-file">
-                             <button class="dropdown-item btn-show-folderCreate-modal" data-toggle="modal" @click.prevent="isAddFolder = true">폴더</button>
+                        <button class="btn btn-xs btn-outline-light dropdown-toggle-new" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" @click="toggleDropdownDefault">New</button>
+                        <div class="dropdown-menu dropdown-new-menu" v-show="isOpenDropdownDefault">
+                             <button class="dropdown-item btn-show-folderCreate-modal" data-toggle="modal" @click.prevent="isAddFolder = true"> 폴더</button>
+                             <div class="dropdown-item">
+                                 <label for="uploadFile" style="cursor: pointer;">파일 업로드</label>
+                                 <input type="file" id="uploadFile" ref="files" @change="onUploadFile" multiple style="position:absolute;width:1px;height:1px;padding:0;margin:-1px;overflow:hidden;clip:rect(0,0,0,0);border:0">
+                             </div>
+                            <a href="" class="dropdown-item"> 폴더 업로드</a>
                         </div>
                         
                      </div>
-                </div>
-                <div class="dropdown-wrapper">
-                    <div class="dropdown">
-                        <button class="btn btn-xs btn-block btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" @click.prevent="dropdownToggle">Upload</button>
-                        <div class="dropdown-menu dropdown-file">
-                            <label for="uploadFile" class="dropdown-item" style="cursor: pointer;">파일</label>
-                            <input type="file" id="uploadFile" class="btn-upload" multiple style="position:absolute;width:1px;height:1px;padding:0;margin:-1px;overflow:hidden;clip:rect(0,0,0,0);border:0">
-                            <a href="" class="dropdown-item">폴더</a>
-                        </div>
-                    </div>
                 </div>
             </div>
             <!-- Sidebar Navigation -->
@@ -30,15 +25,14 @@
                 <span class="dt-side-nav__text">MY DRIVE</span>
                 </li>
                 <!-- /menu header -->
-        
                 <!-- Menu Item -->
-                <li class="dt-side-nav__item open selected" style="position: relative;">
-                <div class="folder-tree">
-                    <router-link to="/disk/mydisk" class="dt-side-nav__link"><i class="far fa-desktop"></i><span class="dt-side-nav__text">내 디바이스</span></router-link>
-                </div>
+                <li class="dt-side-nav__item">
+                    <div class="folder-tree">
+                        <FolderTreeItem class="item" :item="folderTree" @make-folder="makeFolder"></FolderTreeItem>
+                    </div>
                 </li>
                 <li class="dt-side-nav__item">
-                    <router-link to="/disk/recents" class="dt-side-nav__link"><i class="far fa-clock"></i><span class="dt-side-nav__text">최신</span></router-link>
+                    <router-link to="/disk/recents" class="dt-side-nav__link"><i class="far fa-clock"></i><span class="dt-side-nav__text">최근</span></router-link>
                 </li>
                 <li class="dt-side-nav__item">
                     <router-link to="/disk/starred" class="dt-side-nav__link"><i class="far fa-star"></i><span class="dt-side-nav__text">중요</span></router-link>
@@ -68,56 +62,102 @@
             <!-- /sidebar navigation -->
         
             </div>
-            <AddFolder v-if="isAddFolder" @close="isAddFolder=false"/>
+            <ModalInput title="새폴더" ok="만들기" v-if="isAddFolder" @close="isAddFolder=false" @submit="onAddFolder"/>
     </aside>
     <!-- /sidebar -->
 
 </template>
 
 <script>
-import AddFolder from "@/components/AddFolder.vue";
+import {mapActions, mapMutations, mapState} from 'vuex';
+import ModalInput from "@/components/ModalInput.vue";
+import FolderTreeItem from "@/components/FolderTreeItem.vue";
 
 export default {
     components:{
-        AddFolder
+        ModalInput,
+        FolderTreeItem
     },
     data(){
         return{
-            isAddFolder: false
-
+            isAddFolder: false,
+            inputFolderName : '',
         }
     },
-    mounted(){
-        this.init();
-        this.setupClickOutside();
+    created(){
+        this.fetchTreeData();
     },
+    computed:{
+        ...mapState(['openedFolder', 'rootFolderId','folderTree','isOpenDropdownDefault']),
+        invalidInput(){
+            return !this.inputFolderName.trim()
+        }
+    },
+  
     methods:{
-        init(){
-            Array.from(document.querySelectorAll(".dropdown-menu"), (el) => {
-                el.style.display = 'none';
+        ...mapActions(['ADD_FOLDER','UPLOAD_FILE','FETCH_FOLDER_TREE']),
+        ... mapMutations(['SET_IS_OPEN_DROPDOWN_DEFAULT', 'SET_IS_OPEN_DROPDOWN']),
+        onAddFolder(value){
+            this.inputFolderName = value;
+            if(this.invalidInput) return;
+            
+            this.ADD_FOLDER({
+                parentId : this.openedFolder.folderId,
+                folderName : value
+            }).catch(err => {
+                console.error(err);
+            }).finally(()=>{
+                this.isAddFolder = false;
             })
         },
-        setupClickOutside(){
-            document.querySelector('body').addEventListener('click', e =>{
-                Array.from(document.querySelectorAll(".dropdown-menu"), (el) => {
-                    if(e.target.nextSibling == el) return;
-                    el.style.display = 'none';
-                })
-            })
+
+        toggleDropdownDefault(){
+            this.SET_IS_OPEN_DROPDOWN(false);
+            this.isOpenDropdownDefault === false ? this.SET_IS_OPEN_DROPDOWN_DEFAULT(true) : this.SET_IS_OPEN_DROPDOWN_DEFAULT(false);
         },
-        dropdownToggle(e){
-            if(e.target.nextSibling.style.display == 'none'){
-                e.target.nextSibling.style.display = 'block'
-            }else{
-                e.target.nextSibling.style.display = 'none';
+        onUploadFile(){
+            let formData = new FormData();
+            let files = this.$refs.files.files;
+
+            for(let i=0; i < files.length;i++)
+            {
+                formData.append(`files[${i}]`, files[i] );
             }
+
+            formData.append("folderId", this.openedFolder.folderId)
+
+            this.UPLOAD_FILE({
+                file : formData
+            }).finally(() => {
+                //this.$refs.menu2.style.display = 'none';
+            })
         },
+        fetchTreeData(){
+            this.FETCH_FOLDER_TREE()
+        },
+        makeFolder: function (item) {
+            //Vue.set(item, 'children', []);//동적으로 반응형 속성을 추가
+        }
+       
         
     }
 }
 </script>
 <style lang="scss">
-    .dropdown-menu{
-        display: initial
-    }
+ .toggleFolderTree  {
+    position: absolute;
+    top: 8px;
+    left: 14px;
+    z-index: 10;
+ }
+
+ .dropdown-menu{
+     display: initial;
+ }
+
+ .dropdown-toggle-new{
+     width: 180px;
+     height: 35px;
+     border-radius: 50px;
+ }
 </style>
