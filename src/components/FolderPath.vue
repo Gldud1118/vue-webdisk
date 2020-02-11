6496a30b08d68ef24e6b37cf739952c2cb743b26
이희영 <gl171111@gmail.com>
2020-02-11 09:18:49 +0900

    현재 이동 폴더 path를 보여주는 FolderPath 컴포넌트 생성

---
+++ src/components/FolderPath.vue
@@ -0,0 +1,11 @@
+<template>
+    <router-link :to="`/disk/folder/${data.folderId}`" class="folder-path-item">
+        <span class="path mr-3" :data-id="data.folderId">{{data.folderName}}</span><span class="mr-3" v-if="index != count-1">></span>
+    </router-link>
+</template>
+
+<script>
+export default {
+    props : ['data', 'index', 'count']
+}
+</script>
