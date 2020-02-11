9ec19fc8ecaee66ba2fc1a24fcb2ff6fa980d5d1
이희영 <gl171111@gmail.com>
2020-02-11 09:31:41 +0900

    중요표시된 파일만 보여주는 중요파일함 컴포넌트, 바로가기 기능도 제공

---
+++ src/views/Starred.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="filemgr-wrapper">
+        <div class="filemgr-content">
+            <div class="filemgr-content-body ps ps--active-y">
+                <FolderContents :data="starredDisk"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import {mapActions, mapMutations, mapState} from 'vuex';
+import FolderContents from "@/components/FolderContents.vue";
+
+export default {
+    components:{
+        FolderContents
+    },
+    computed:{
+        ...mapState(['starredDisk', 'isStarredUpdate'])
+    },
+    created(){
+        this.fetchData();
+    },
+    methods: {
+        ...mapActions(['FETCH_STARRED_DISK']),
+        fetchData(){
+            if(this.isStarredUpdate){
+                this.FETCH_STARRED_DISK();
+            }
+        }
+    },
+}
+</script>
